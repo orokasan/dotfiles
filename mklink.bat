c9a71bc656cf05feda7161ef811c2bb327d4c77e
orokasan <fuu989@gmail.com>
2019-03-14 15:18:21 +0900

    add syntax/plugin link

--- mklink.bat
+++ mklink.bat
@@ -3,4 +3,6 @@ mklink %HOMEPATH%"\_gvimrc" %HOMEPATH%"\dotfiles\_gvimrc"
 mklink /D %HOMEPATH%"\vimfiles\ftdetect" %HOMEPATH%"\dotfiles\ftdetect"
 mklink /D %HOMEPATH%"\vimfiles\indent" %HOMEPATH%"\dotfiles\indent"
 mklink /D %HOMEPATH%"\vimfiles\colors" %HOMEPATH%"\dotfiles\colors"
+mklink /D %HOMEPATH%"\vimfiles\syntax" %HOMEPATH%"\dotfiles\syntax"
+mklink /D %HOMEPATH%"\vimfiles\Plugin" %HOMEPATH%"\dotfiles\Plugin"
 exit 0
